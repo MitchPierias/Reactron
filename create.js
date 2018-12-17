eaad6b8353567f5fefa0d39f3a1d918cd83a727d
Mitch Pierias <mitch@pierias.com>
2018-12-18 00:29:21 +1000

    Implemented repo cloning
    - [x] Creates directory for project name
    - [x] Repository clones into directory
    - [ ] package.json reconfigure fails

--- create.js
+++ create.js
@@ -1,13 +1,65 @@
 const colors = require('colors');
+const path = require('path');
 
 async function createBoilerplate(args) {
     const spinner = require('./spinner');
     const projectName = args['--name'] || args._[0];
+    const outputPath = path.resolve(process.cwd(),projectName);
 
-    console.log(colors.cyan("Creating project '"+projectName+"'"));
+    spinner.create('Searching for repository...');
+    try {
+        await getRepositoryMeta('React-Electron-Boilerplate');
+        spinner.end();
+    } catch (error) {
+        spinner.fail('Failed to find repository')
+    }
 
-    getPackageManager();
-    console.log(colors.green("Success"));
+    spinner.create('Creating directory');
+    try {
+        await require('make-dir')(outputPath)
+        spinner.end();
+    } catch (err) {
+        spinner.fail('Directory exists');
+    }
+
+    spinner.create('Downloading template...');
+    try {
+        await cloneRepository('https://codeload.github.com/MitchPierias/React-Electron-Boilerplate/tar.gz/master', outputPath);
+        spinner.end();
+    } catch(err) {
+        spinner.fail('Failed to clone template');
+    }
+
+    spinner.create('Configuring project...');
+    try {
+        // Update package file
+        const pkg = require(path.resolve(__dirname, `${projectName}/package.json`));
+        pkg.name = projectName;
+        pkg.version = "1.0.0";
+        // Save package file
+        const { writeFileSync } = require('fs');
+        writeFileSync(path.resolve(__dirname, `${projectName}/package.json`), JSON.stringify(pkg), 'utf8');
+        // Output completion message
+        const cmd = (await getPackageManager() === 'yarn') ? 'yarn && yarn start' : 'npm install && npm start'
+        spinner.end(`Run \`${cmd}\` inside of "${projectName}" to start the app`)
+    } catch (error) {
+        console.log(error);
+        spinner.fail('Failed to configure project');
+    }
+}
+
+async function getRepositoryMeta(repo) {
+    const Github = require('@octokit/rest')();
+    return await Github.repos.get({owner: 'MitchPierias', repo})
+    .then(response => response.data)
+    .catch(error => false);
+}
+
+async function cloneRepository(repoUrl, outputPath) {
+    const got = require('got');
+    const tar = require('tar');
+
+    await got.stream(repoUrl).pipe(tar.x({ cwd:outputPath, strip:1 }));
 }
 
 async function getPackageManager() {
